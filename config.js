d1e86f36365f19a1c6c05506d4a66556b424dd79
tranyingminh <93786662+tranyingminh@users.noreply.github.com>
2024-02-14 15:56:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chó Ngọc mỏ hỗn';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chức mừng Valentine';
+const titleCard = 'Thí Ngọc đáng iu';
+const contentCard = 'Chúc em bé Thí Ngọc Valentine thiệt là dui dẻ, chờ em bé của anh lên sì gòn mình cũng nhau đi cháy phố nèo... Iu emmmm♥♥♥';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'cuoiia.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
